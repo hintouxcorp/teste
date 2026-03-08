a0eb55feb4b484abbb7ea99e74add36dc30c98ad
hintou <jonas.techdeveloper@gmail.com>
2026-03-08 11:53:03 -0300

    feat: sistema de autenticacao

---
+++ index.js
@@ -0,0 +1,17 @@
+const numero1 = 300;
+const numero2 = 400;
+
+const soma = (a, b) => {
+    return a + b;
+};
+
+const subtrair = (a, b) => {
+    return a - b;
+};
+
+const multiplicar = (a, b) => {
+    return a * b;
+};
+
+const valorTotal = soma(numero1, numero2);
+console.log(`O valor total é ${valorTotal}!`);
